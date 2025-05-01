1dd7328af4b6b1afd4f9ee16c83f22f9b51386e2
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-05-01 16:55:33 +0700

    Create file & acces modifier

---
+++ TugasTambahan.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Segitiga
+{
+private:
+    float luas(int a, int t)
+    {
+        return 0.5 * a * t;
+    }
+
+public:
+    void inputData()
+    {
+    }
+};
+
+int main()
+{
+}
