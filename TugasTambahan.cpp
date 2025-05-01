511fb56035844074e225a31c73223af20d2b61c3
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-05-01 17:02:54 +0700

    add if for odd & output

--- TugasTambahan.cpp
+++ TugasTambahan.cpp
@@ -4,7 +4,6 @@ using namespace std;
 class Segitiga
 {
 private:
-    int alas, tinggi;
     float luas(int a, int t)
     {
         return 0.5 * a * t;
@@ -13,11 +12,22 @@ private:
 public:
     void inputData()
     {
+        int alas, tinggi;
         cout << "Masukkan Alas Segitiga : ";
         cin >> alas;
 
         cout << "Masukkan Tinggi Segitiga : ";
         cin >> tinggi;
+
+        if (alas % 2 != 0)
+        {
+            cout << "Alas yang dimasukkan adalah bilangan ganjil, luas tidak dapat dihitung." << endl;
+        }
+        else
+        {
+            float hasilLuas = luas(alas, tinggi);
+            cout << "Luas Segitiga : " << hasilLuas << " cm" << endl;
+        }
     }
 };
 
